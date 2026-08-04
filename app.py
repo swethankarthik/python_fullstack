ecdd7a3583b6d34b0546deecfed5ecc1ce460527
swethankarthik <swethankarthik.avvari@gmail.com>
2026-08-04 14:55:37 +0530

    Folder Allocation

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, static_folder='static', template_folder='templates')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/courses')
+def courses():
+    return render_template('courses.html')
+
+@app.route('/trainers')
+def trainers():
+    return render_template('trainers.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
